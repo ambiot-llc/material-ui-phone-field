2db76a7ca069380a71a008838cf19975b339378f
Randex <randexdev@gmail.com>
2020-03-12 20:00:39 +0300

    Add some initial render tests

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -8,4 +8,36 @@ describe('it', () => {
     ReactDOM.render(<PhoneField value="" onChange={() => {}} />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
+
+  it('renders with default country', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <PhoneField value="" onChange={() => {}} defaultCountry="RU" />,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('renders with different language', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <PhoneField
+        value=""
+        onChange={() => {}}
+        defaultCountry="RU"
+        language="ru"
+      />,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('renders with different language without default country', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <PhoneField value="" onChange={() => {}} language="ru" />,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
